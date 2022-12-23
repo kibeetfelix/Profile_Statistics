4c143f04b3cb969382200d58a1c95f49ae7ba1f6
kibeetfelix <felixkibeet@gmail.com>
2022-12-24 01:16:54 +0300

    incrementing and setting the timeout

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ const counterEl = document.querySelectorAll(".counter");
 
 counterEl.forEach((counterEl) => {
     counterEl.innerText = "0";
-    increamentCounter()
-    function increamentCounter() {
-        const currentNum = +counterEl.innerText
-        const dataCeil = counterEl.getAttribute("data-ceil")
+    incrementCounter()
+    function incrementCounter() {
+        let currentNum = +counterEl.innerText;
+        const dataCeil = counterEl.getAttribute("data-ceil");
+        const increment = dataCeil / 15
+        currentNum = Math.floor(currentNum + increment)
+        if (currentNum < dataCeil) {
+            counterEl.innerText = currentNum
+            setTimeout(incrementCounter, 50)
+        } else {
+            counterEl.innerText = dataCeil
+        }
     }
 });
